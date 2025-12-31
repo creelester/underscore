3fc3277736f4628344b4e21bc525369ef716eaa5
Cristina Lester <cristina.deelester@gmail.com>
2025-12-31 11:17:45 -0800

    added auth with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,28 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import { type Metadata } from 'next';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import { Geist, Geist_Mono } from 'next/font/google';
+// @ts-ignore: missing type declarations for CSS side-effect import
+import './globals.css';
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-  title: "Underscore - AI Soundtrack for Your Books",
-  description: "Creates a soundtrack for your books using AI",
+  title: 'Underscore',
+  description: 'The soundtrack to your story',
 };
 
 export default function RootLayout({
@@ -12,8 +31,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <header className='flex justify-end items-center p-4 gap-4 h-16'>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className='bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer'>
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
